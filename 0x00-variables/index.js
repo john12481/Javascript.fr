a277590994f3617d5a21603eafbd233d36f5f1e3
john 12481 <akposj423@gmail.com>
2024-09-19 12:22:49 +0100

    Add all files

---
+++ 0x00-variables/index.js
@@ -0,0 +1,10 @@
+// Case sensitive
+
+const userName = "Favour";
+const UserName = "Kome";
+const username = "maro";
+console.log(userName);
+console.log(UserName);
+console.log(username);
+
+// This code will run because each of the variables are different (case sensitive)
---
+++ 0x01-Datatypes/index.js
@@ -0,0 +1,14 @@
+let is_Admin = true;
+let is_user = false;
+
+console.log(is_Admin);
+console.log(is_user);
+
+results = typeof is_Admin;
+console.log(results);
+
+let name;
+console.log(name);
+
+let name2 = null;
+console.log(name2);
