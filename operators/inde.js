32cea3af0448c6e0e0d44e51e12f74dc83c8e356
john 12481 <akposj423@gmail.com>
2024-09-19 13:56:59 +0100

    Please add files

---
+++ operators/inde.js
@@ -0,0 +1,34 @@
+//  Arithmetic operators
+let a = 4;
+let b = 3;
+
+let addition = a + b;
+let sub = a - b;
+console.log("addition of " + a + "and " + b + "is:" + addition);
+
+let c = 3;
+let d = 4;
+
+let multiply = c * d;
+let divide = c / d;
+console.log(" Multiplication  of " + c + " and " + d + " is: " + multiply);
+console.log(" Division of " + c + " and " + d + " is: " + divide);
+
+// Number of days
+let days = 3;
+let numberofHours = days * 24;
+let numberofMin = days * 60;
+let numberofSec = numberofMin * 60;
+
+console.log(
+  " There Are " +
+    numberofHours +
+    " Hours," +
+    numberofMin +
+    " Mintues," +
+    " and " +
+    numberofSec +
+    " Seconds in " +
+    days +
+    " Days "
+);
