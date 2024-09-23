fd78c72bd8b3e96efcfc47882bdf3238e9a72d47
john 12481 <akposj423@gmail.com>
2024-09-23 11:54:54 +0100

    Add all

---
+++ 0x01-Datatypes/Assignments/0x04-interest/index.js
@@ -0,0 +1,15 @@
+let principal = 1000;
+let rate = 5;
+let time = 3;
+let simpleInterest = (principal * rate * time) / 100;
+
+console.log(
+  "For a Principal of " +
+    principal +
+    " at a rate of " +
+    rate +
+    " % over " +
+    time +
+    " years, the simple interest is = " +
+    simpleInterest
+);
---
+++ 0x01-Datatypes/Assignments/0x05-petrol/index.js
@@ -0,0 +1,6 @@
+let milesDriven = 300;
+let fuelEfficiency = 25;
+let gallonsUsed = milesDriven / fuelEfficiency;
+
+console.log(" You have driven" + milesDriven + " miles");
+console.log(" You have used " + gallonsUsed + " Gallons of Fuel.");
