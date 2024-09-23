0dc0f36948a33ab95c9ebe4f3cf3410292c95dd4
john 12481 <akposj423@gmail.com>
2024-09-23 13:37:02 +0100

    add all again

--- operators/inde.js
+++ operators/inde.js
@@ -32,3 +32,57 @@ console.log(
     days +
     " Days "
 );
+
+// Increamental
+let num = 4;
+num++;
+console.log(num);
+
+let num2 = 5;
+num2--;
+console.log(num2);
+
+let num3 = 6;
+console.log(num++);
+
+console.log(num3);
+
+let num4 = 8;
+console.log(++num4);
+
+let num5 = 10;
+num5 = num5 + 5;
+console.log(num5);
+// or
+num5 += 5;
+
+// comparison opeartors
+let num6 = 5;
+let num7 = "5";
+
+console.log(num6 == num7);
+
+console.log(num6 === num7);
+
+// Not equal to
+let num8 = 5;
+let num9 = 5;
+
+console.log(num8 !== num9);
+
+// greater than and less than
+let num10 = 14;
+let num11 = 10;
+
+console.log(num10 > num11);
+console.log(num10 < num11);
+
+// Logical Operators
+let isStudent = true;
+let hasPaid = true;
+
+console.log(isStudent && hasPaid);
+console.log(isStudent || hasPaid);
+
+let isHungry = true;
+console.log(isHungry);
